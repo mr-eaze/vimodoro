e96aca18e4479fef3d46fd42c0425b4ae50a7a41
Adam Gallinat <adamgallinat@gmail.com>
2015-03-31 16:31:56 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var application_root = __dirname,
+    express          = require('express'),
+    bodyParser       = require('body-parser'),
+    path             = require('path'),
+    logger           = require('morgan'),
+    models           = require('./models');
+
+var app = express();
+
+// Server Configuration
+app.use(logger('dev'));
+app.use( bodyParser() );
+app.use( express.static( path.join( application_root, 'public' )))
+app.use( express.static( path.join( application_root, 'browser' )))
+
+// Routes
+
+// Export app as module
+module.exports = app;
