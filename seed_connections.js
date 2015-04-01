73019bebca3a78eff5f8476e9ca82408ce325eca
Eric Vince <eric.vince.ltd@gmail.com>
2015-04-01 13:41:53 -0400

    seed_connections.js is working

--- seed_connections.js
+++ seed_connections.js
@@ -3,15 +3,20 @@ var models  = require('./models');
 var User    = models.users;
 var Keyword = models.keywords;
 
-var seedConnections = function () {
-	User.findOne( {
-		where: {
-			name: 'Adam Gallinat'
-		}
-	}).then(function (user) {
-		Keyword.findOne(1)
-			.then(function (keyword) {
-				user.addKeyword(keyword);
-			});
-	});
+var seedConnections = function () { 
+	User
+		.findOne( { 
+			where: { 
+				name: 'Adam Gallinat' 
+			} 
+		})
+		.then( function (user) { 
+			Keyword
+				.findOne(1)
+				.then(function (keyword) { 
+					user.addKeyword(keyword) 
+				}); 
+		});
 };
+
+seedConnections();
